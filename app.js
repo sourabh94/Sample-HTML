b6347db15faf8e667cbdc7a91a2ecef74807fa62
Tripstr <ladiable8@gmail.com>
2017-07-20 17:35:28 +0530

    error resolver

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ app.use('/admin', users);
 
   // Here we require the prerender middleware that will handle requests from Search Engine crawlers 
   // We set the token only if we're using the Prerender.io service 
-  app.use(prerender); 
-app.get('/*', function(req, res){ 
+app.use(prerender); 
+app.get('*', function(req, res){ 
   res.sendfile('./public/index.html'); 
 });
 app.use(function (req, res, next) {
